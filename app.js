9bc99282bddfaff574e81d3f33e0d02d98796cd0
Sérgio Moura <smouracalmon@gmail.com>
2021-09-05 09:20:25 -0300

    Adicionaro Cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', UsuariosRouter);
 app.use('/', TokenExists, VerifyToken, ContatosRouter);
